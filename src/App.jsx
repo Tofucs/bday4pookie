10e0859c4f68f9fd5fb76213f51ffffdada4c708
ajh365 <ajh365@cornell.edu>
2024-05-18 18:41:01 -0400

    text showing up for pookie

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import catCuddle from './assets/cuddling.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [messageDisplayed, setMessageDisplayed] = useState(false);
+  const [imagesDisplayed, setImagesDisplayed] = useState(false);
+
+  const handleButtonClick = () => {
+    setMessageDisplayed(true);
+    setImagesDisplayed(true);
+    setTimeout(() => {
+      setImagesDisplayed(false);
+    }, 5000); // Images will fade out after 5 seconds
+  };
+
+  const images = [
+    './assets/mypookie.png',
+    './assets/mypookie.png',
+    './assets/mypookie.png',
+  ]
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="content">
+        {!messageDisplayed ?
+          <button onClick={handleButtonClick} className="button">
+            <span className='button-text'>Guess what day it is :3</span>
+            <img src={catCuddle} alt="Orange Cat" className="cat-image" />
+          </button>
+          :
+          <h1>Happy Birthday Pookie! 🎉</h1>
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      {!messageDisplayed ?
+        <p className="read-the-docs">
+          click the text!
+        </p> :
+        <p className="read-the-docs">
+          i love u :3
+        </p>}
+    </div>
   )
 }
 
